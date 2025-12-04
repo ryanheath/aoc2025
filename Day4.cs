22e3a758f51893a09fe610aa805c5a0927d7520e
Ryan Heath <ryan.q.heath@gmail.com>
2025-12-04 08:38:35 +0100

    Day 4 - part 2

--- Day4.cs
+++ Day4.cs
@@ -22,50 +22,75 @@
                 @.@.@@@.@.
                 """.ToLines();
             Part1(input).Should().Be(13);
-            Part2(input).Should().Be(0);
+            Part2(input).Should().Be(43);
         }
 
         void Compute()
         {
             var input = File.ReadAllLines($"{day.ToLowerInvariant()}.txt");
             Part1(input).Should().Be(1480);
-            Part2(input).Should().Be(0);
+            Part2(input).Should().Be(8899);
         }
 
-        int Part1(string[] grid)
+        int Part1(string[] lines)
         {
+            var grid = ParseGrid(lines);
             var h = grid.Length;
             var w = grid[0].Length;
             var accessibleCount = 0;
 
             for (var y = 0; y < h; y++)
             for (var x = 0; x < w; x++)
-                if (grid[y][x] == '@' && CountSurrounding(y, x) < 4)
-                {
-                    accessibleCount++;
-                }
+                if (IsAccessible(grid, y, x)) accessibleCount++;
 
             return accessibleCount;
-
-            int CountSurrounding(int y, int x)
-            {
-                var sy = y - 1 < 0 ? 0 : y - 1;
-                var ey = y + 1 >= h ? h - 1 : y + 1;
-                var sx = x - 1 < 0 ? 0 : x - 1;
-                var ex = x + 1 >= w ? w - 1 : x + 1;
-
-                var rolls = 0;
-
-                for (var iy = sy; iy <= ey; iy++)
-                for (var ix = sx; ix <= ex; ix++)
-                    if (iy == y && ix == x)
-                        continue;
-                    else if (grid[iy][ix] == '@')
-                        rolls++;
-
-                return rolls;
-            }
         }
-        int Part2(string[] grid) => 0;
+        int Part2(string[] lines)
+        {
+            var grid = ParseGrid(lines);
+            var h = grid.Length;
+            var w = grid[0].Length;
+            var removed = 0;
+            var toBeRemoved = new List<(int y, int x)>();
+
+            do
+            {
+                for (var y = 0; y < h; y++)
+                for (var x = 0; x < w; x++)
+                    if (IsAccessible(grid, y, x)) toBeRemoved.Add((y, x));
+
+                if (toBeRemoved.Count == 0) break;
+
+                removed += toBeRemoved.Count;
+                foreach (var (y, x) in toBeRemoved)
+                    grid[y][x] = '.';
+                toBeRemoved.Clear();
+
+            } while (true);
+
+            return removed;
+        }
+
+        static bool IsAccessible(char[][] grid, int y, int x) => grid[y][x] == '@' && CountSurrounding(grid, y, x) < 4;
+
+        static char[][] ParseGrid(string[] lines) => [.. lines.Select(line => line.ToCharArray())];
+        static int CountSurrounding(char[][] grid, int y, int x)
+        {
+            var sy = y - 1 < 0 ? 0 : y - 1;
+            var ey = y + 1 >= grid.Length ? grid.Length - 1 : y + 1;
+            var sx = x - 1 < 0 ? 0 : x - 1;
+            var ex = x + 1 >= grid[0].Length ? grid[0].Length - 1 : x + 1;
+
+            var rolls = 0;
+
+            for (var iy = sy; iy <= ey; iy++)
+            for (var ix = sx; ix <= ex; ix++)
+                if (iy == y && ix == x)
+                    continue;
+                else if (grid[iy][ix] == '@')
+                    rolls++;
+
+            return rolls;
+        }
     }
 }
